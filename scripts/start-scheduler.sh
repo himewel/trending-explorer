28cc9364adab8567acc1d44d9c1ca6e182d2269c
Welbert Castro <himewel@users.noreply.github.com>
2022-01-11 22:40:36 -0300

    Add docker base files

---
+++ scripts/start-scheduler.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+echo "Waiting airflow webserver to launch on airflow_webserver:8080..."
+while ! nc -z airflow-webserver 8080; do
+    sleep 1
+done
+
+echo "Clear old .err and .pid files..."
+rm $AIRFLOW_HOME/*.pid $AIRFLOW_HOME/*.err
+
+echo "Starting service..."
+airflow scheduler
---
+++ scripts/start-webserver.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+echo "Waiting postgres to launch on airflow-postgres:5432..."
+while ! nc -z airflow-postgres 5432; do
+    sleep 1
+done
+
+echo "Setting up meta database..."
+airflow db upgrade
+
+echo "Creating default user..."
+airflow users create \
+    --role Admin \
+    --username admin \
+    --password admin \
+    --firstname First \
+    --lastname Name \
+    --email name@domain.com
+
+echo "Clear old .err and .pid files..."
+rm $AIRFLOW_HOME/*.pid $AIRFLOW_HOME/*.err
+
+echo "Starting service..."
+airflow webserver
