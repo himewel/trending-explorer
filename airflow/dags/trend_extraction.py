236a8d46abc2ea2ebfe6ea7b5335e1b06ff7165b
Welbert Castro <himewel@users.noreply.github.com>
2022-01-14 18:42:51 -0300

    Add yaml config file

--- airflow/dags/trend_extraction.py
+++ airflow/dags/trend_extraction.py
@@ -1,13 +1,11 @@
 from datetime import datetime
 
+import pendulum
+import yaml
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 
 from include import TweepyExtractor, TweetIngestion, TweetRelease
-
-_LANDING_PATH = "/data/landing"
-_RAW_PATH = "/data/raw"
-_TRUSTED_PATH = "/data/trusted"
 
 default_args = {
     "depends_on_past": True,
@@ -15,32 +13,44 @@ default_args = {
     "wait_for_downstream": True,
 }
 
-with DAG(
-    dag_id="trending-extraction",
-    default_args=default_args,
-    description="Run automated data ingestion of tweets from trending topics",
-    max_active_runs=1,
-    schedule_interval="*/15 * * * *",
-    start_date=datetime(2022, 1, 10),
-    tags=["Twitter", "Spark", "Tweepy", "DeltaLake"],
-) as dag:
-    extractor = TweepyExtractor(path=_LANDING_PATH)
-    extraction_task = PythonOperator(
-        task_id="extract_from_api",
-        python_callable=extractor.fetch,
-        op_kwargs={"topic": "#covid19", "max_results": 10},
-    )
+with open("dags/config.yaml") as stream:
+    config = yaml.safe_load(stream)
 
-    ingestion = TweetIngestion(landing_path=_LANDING_PATH, raw_path=_RAW_PATH)
-    ingestion_task = PythonOperator(
-        task_id="ingest_to_raw",
-        python_callable=ingestion.evaluate,
-    )
+for topic, values in config["topics"].items():
+    start_date = pendulum.instance(values["start-date"])
+    schedule_interval = values["schedule-interval"]
+    max_results = values["max-results"]
+    landing_path = values["landing-path"]
+    raw_path = values["raw-path"]
+    trusted_path = values["trusted-path"]
 
-    release = TweetRelease(raw_path=_RAW_PATH, trusted_path=_TRUSTED_PATH)
-    release_task = PythonOperator(
-        task_id="release_to_trusted",
-        python_callable=release.evaluate,
-    )
+    with DAG(
+        dag_id=f"{topic}-trending-extraction",
+        default_args=default_args,
+        description="Run automated data ingestion of tweets from trending topics",
+        max_active_runs=1,
+        schedule_interval=schedule_interval,
+        start_date=start_date,
+        tags=["Twitter", "Spark", "Tweepy", "DeltaLake"],
+    ) as dag:
+        extractor = TweepyExtractor(path=landing_path)
+        extraction_task = PythonOperator(
+            task_id="extract_from_api",
+            python_callable=extractor.fetch,
+            op_kwargs={"topic": f"#{topic}", "max_results": max_results},
+        )
 
-    extraction_task >> ingestion_task >> release_task
+        ingestion = TweetIngestion(landing_path=landing_path, raw_path=raw_path)
+        ingestion_task = PythonOperator(
+            task_id="ingest_to_raw",
+            python_callable=ingestion.evaluate,
+        )
+
+        release = TweetRelease(raw_path=raw_path, trusted_path=trusted_path)
+        release_task = PythonOperator(
+            task_id="release_to_trusted",
+            python_callable=release.evaluate,
+        )
+
+        extraction_task >> ingestion_task >> release_task
+        globals()[dag.dag_id] = dag
